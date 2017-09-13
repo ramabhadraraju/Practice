8c4be3ada8c2513c38b32f2ac7edd410528f8c00
ramabhadraraju <31580375+ramabhadraraju@users.noreply.github.com>
2017-09-12 17:48:06 -0700

    iris
    knn and logistic regression for iris data set. accuracy vs knn plotted using matplot lib

--- iris.py
+++ iris.py
@@ -1,13 +1,45 @@
-from sklearn import datasets
-iris = datasets.load_iris()
+#Simple iris dataset classification using Knn and logistic regression.
+#Load datasets
+from sklearn.datasets import load_iris
+iris= load_iris()
+
+#separate features and labels
 x= iris.data
 y= iris.target
 
+#Split data into train and test
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=.5)
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.4)
+
+#Logistic Regression
+from sklearn.linear_model import LogisticRegression
+cls = LogisticRegression()
+cls.fit(x_train,y_train)
+z=cls.predict(x_test)
+#accuracy prediction
+from sklearn import metrics
+print(metrics.accuracy_score(y_test,z))
+
+# knn for different depths
 from sklearn.neighbors import KNeighborsClassifier
-my_classifier= KNeighborsClassifier()
-my_classifier.fit(x_train,y_train)
-predictions= my_classifier.predict(x_test)
-from sklearn.metrics import accuracy_score
-print (accuracy_score(y_test,predictions))
+scores=[]
+k_range=range(1,26)
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(x_train, y_train)
+    knn_pred = knn.predict(x_test)
+    # import metrics to calculate accuracy
+    from sklearn import metrics
+    accuracy=metrics.accuracy_score(y_test, knn_pred)
+    scores.append(accuracy)
+print(scores)
+#display the best k using matplotlib
+import matplotlib.pyplot as plt
+plt.plot(k_range,scores)
+plt.xlabel("k values")
+plt.ylabel("accuracy")
+plt.show()
+
+
+
+
